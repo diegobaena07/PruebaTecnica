87f26b4472fe1e8572348ad57cf6b7d076ab2620
diegobaena07 <145508134+diegobaena07@users.noreply.github.com>
2023-11-15 22:05:49 -0500

    Ajustes para comenzar todo el frontend

---
+++ smartwater/src/app/pages/PaginaPrincipal/PaginaPrincipal.tsx
@@ -0,0 +1,15 @@
+import "./PaginaPrincipal.css";
+import { FC } from "react";
+
+const PaginaPrincipal: FC = () => {
+
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export { PaginaPrincipal }
