74b24f405c7c761a1027ba9c6cbfd0821b0a0e53
Diego Baena <baena1907@gmail.com>
2023-11-16 14:57:35 -0500

    Cuadros informativos terminados

--- smartwater/src/app/pages/PaginaPrincipal/PaginaPrincipal.tsx
+++ smartwater/src/app/pages/PaginaPrincipal/PaginaPrincipal.tsx
@@ -1,13 +1,49 @@
 import "./PaginaPrincipal.css";
 import { Sidebar } from "../components/Sidebar/Sidebar";
 import { FC } from "react";
+import { CuadroInformativo } from "../components/CuadroInformativo/CuadroInformativo";
 
 const PaginaPrincipal: FC = () => {
 
     return (
         <>
-            <div>
-                <Sidebar/>
+            <div className="container">
+                <Sidebar />
+                <div className="contenido">
+                    <div className="linea-1">
+                        <div className="home">
+                            <img src="./home-icon.svg" className="img-HomeInicio" />
+                            <div className="letras-home">
+                                <span>Inicio</span>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="./Notificacion-icon.svg" />
+                        </div>
+                    </div>
+                    <div className="Cuadros-informativos">
+                        <CuadroInformativo
+                            titulo="Clientes nuevos"
+                            numero="18"
+                            porcentajeVerde="+8.2%" />
+
+                        <CuadroInformativo
+                            titulo="Préstamos activos"
+                            numero="25"
+                            porcentajeRojo="+8.2%" />
+
+                        <CuadroInformativo
+                            titulo="Pedidos totales"
+                            numero="1,236"
+                            porcentajeVerde="+8.2%" />
+
+                        <CuadroInformativo
+                            titulo="Clientes nuevos"
+                            numero="1,783"
+                            letra="Bs"
+                            porcentajeVerde="+8.2%" />
+                    </div>
+                </div>
             </div>
         </>
     )
