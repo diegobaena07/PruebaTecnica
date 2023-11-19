c3f4edaff3b7738fcd312232411e04b264db92c4
diegobaena07 <145508134+diegobaena07@users.noreply.github.com>
2023-11-18 21:09:52 -0500

    arreglo de carpetas

---
+++ src/app/pages/PaginaPrincipal/PaginaPrincipal.tsx
@@ -0,0 +1,68 @@
+import "./PaginaPrincipal.css";
+import { Sidebar } from "../components/Sidebar/Sidebar";
+import { FC } from "react";
+import { CuadroInformativo } from "../components/CuadroInformativo/CuadroInformativo";
+import { CuadroClientes } from "../components/CuadroClientes/CuadroClientes";
+import { CuadroRealizarPedido } from "../components/CuadroRealizarPedido/CuadroRealizarPedido";
+import { BarChart } from "../components/Barchart/Barchart";
+
+const PaginaPrincipal: FC = () => {
+
+    return (
+        <>
+            <div className="container">
+                <Sidebar />
+                <div className="contenido">
+                    <div className="titulo-1">
+                        <div className="home">
+                            <img src="./home-icon.svg" className="img-HomeInicio" />
+                            <div className="letras-home">
+                                <span>Inicio</span>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="./Notificacion-icon.svg" />
+                        </div>
+                    </div>
+                    <div className="Cuadros-informativos">
+                        <CuadroInformativo
+                            titulo="Clientes nuevos"
+                            numero="18"
+                            porcentajeVerde="+8.2%" />
+
+                        <CuadroInformativo
+                            titulo="Préstamos activos"
+                            numero="25"
+                            porcentajeRojo="+8.2%" />
+
+                        <CuadroInformativo
+                            titulo="Pedidos totales"
+                            numero="1,236"
+                            porcentajeVerde="+8.2%" />
+
+                        <CuadroInformativo
+                            titulo="Clientes nuevos"
+                            numero="1,783"
+                            letra="Bs"
+                            porcentajeVerde="+8.2%" />
+                    </div>
+                    <div className="titulo-2">
+                        <span>Acciones rápidas</span>
+                    </div>
+                    <div className="Cuadros-AccionesRapidas">
+                        <CuadroClientes />
+
+                        <CuadroRealizarPedido />
+
+                        <div className="cuadro-vacio">
+
+                        </div>
+                    </div>
+                    <BarChart/>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export { PaginaPrincipal }
